c006590d76f8fd7ed7c7df76cfd0288e913d60c5
uziiii152 <uzairqaseem162@gmail.com>
2024-03-24 23:05:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answers = await inquirer_1.default.prompt([
+    {
+        message: "enter your num1",
+        type: "number",
+        name: "firstNum"
+    },
+    {
+        message: "enter your num2",
+        type: "number",
+        name: "secondNum"
+    },
+    {
+        message: "which operation do you want to perform!",
+        type: "list",
+        name: "operations",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    }
+]);
+if (answers.operations === "addition") {
+    console.log(answers.firstNum + answers.secondNum);
+}
+else if (answers.operations === "subtraction") {
+    console.log(answers.firstNum - answers.secondNum);
+}
+else if (answers.operations === "multiplication") {
+    console.log(answers.firstNum * answers.secondNum);
+}
+else if (answers.operations === "division") {
+    console.log(answers.firstNum / answers.secondNum);
+}
+else {
+    console.log("your answer is not valid.Please! make sure to use the correct value and type");
+}
